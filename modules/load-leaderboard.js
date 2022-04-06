3f6c4f6cda0221577945c8faa3bd09e104197884
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-06 16:31:51 +0300

    Add get and post API calls

---
+++ modules/load-leaderboard.js
@@ -0,0 +1,16 @@
+import { getScore } from './add-score';
+
+const scoreList = document.querySelector('.current-scores');
+
+export default async () => {
+  scoreList.innerHTML = '<p>Loading Leaderboard...</p>';
+  const leaders = await getScore();
+  const rankedLeaders = leaders.sort((a, b) => b.score - a.score);
+
+  let leaderBoard = '';
+  rankedLeaders.forEach(({ user, score }) => {
+    leaderBoard += `<li class="score-item">${user}: ${score}</li>`;
+  });
+
+  scoreList.innerHTML = leaderBoard;
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import loadLeaderboard from '../modules/load-leaderboard';
+import { addScore } from '../modules/add-score';
+
+const form = document.querySelector('#add-new-score');
+const refreshBtn = document.querySelector('#refresh');
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadLeaderboard();
+});
+
+refreshBtn.addEventListener('click', () => {
+  loadLeaderboard();
+});
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  await addScore();
+  loadLeaderboard();
+  form.reset();
+});
