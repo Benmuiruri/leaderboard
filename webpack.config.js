7d83a43693d544cc7d07b2212b31d854947625c1
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-06 16:54:25 +0300

    Fix webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,8 @@ module.exports = {
   module: {
     rules: [
       {
-       test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
