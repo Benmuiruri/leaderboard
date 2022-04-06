5397c0cafb35dfaffbadd435c5bb35620baadf18
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-06 16:46:39 +0300

    Change to scss

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+       test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
