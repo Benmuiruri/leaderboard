ade3234245451ab605bef494c8177021aba56c71
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-07 12:41:39 +0300

    Update UI css

--- modules/load-leaderboard.js
+++ modules/load-leaderboard.js
@@ -3,13 +3,13 @@ import { getScore } from './add-score';
 const scoreList = document.querySelector('.current-scores');
 
 export default async () => {
-  scoreList.innerHTML = '<p>Loading Leaderboard...</p>';
+  scoreList.innerHTML = '<p class="loading">Loading Leaderboard...</p>';
   const leaders = await getScore();
   const rankedLeaders = leaders.sort((a, b) => b.score - a.score);
 
   let leaderBoard = '';
   rankedLeaders.forEach(({ user, score }) => {
-    leaderBoard += `<li class="score-item"><i class="fa-solid fa-user-astronaut"></i>${user}: ${score}</li>`;
+    leaderBoard += `<li class="score-item"><i class="fa-solid fa-user-astronaut"></i><span class="user-name">${user}</span> <span class="user-score">${score}</span></li>`;
   });
 
   scoreList.innerHTML = leaderBoard;
